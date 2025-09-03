d44c29305c1bb3add0d9f795b7511c975e6aecff
Rahul Negi <75606995+rahul-negi-projects@users.noreply.github.com>
2025-09-04 01:59:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 from backend import importing_data,workflow
-st.secrets['GROQ_API_KEY']
 
 # ---- Streamlit Page ----
 st.set_page_config(page_title="Dashboard Generator", layout="wide")
